07c74646b3b20a179ee31dbab323714dbf457b95
Valeria <valeriakirkbride@gmail.com>
2023-08-28 21:06:49 -0600

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ftCost){
+  if (ftCost <= 400){
+    return 'This one is on me!'
+  }
+  else if (ftCost > 400 && ftCost < 2000){
+    return 'That will be twenty bucks.'
+  }
+  else if (ftCost > 2000 && ftCost < 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (ftCost > 2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityCheck){
+  return cityCheck === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    break;
+    default:
+      return 'Bye.'
+  }
 }
